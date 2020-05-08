58068e2b6b3f5f731f541fdfd44ad42905fe863f
root <root@eac9105acdb4>
2020-05-08 22:28:33 +0000

    in zombie eyes.

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,40 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score =0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer =JOptionPane.showInputDialog("what month of the year has 28 days?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (answer.equalsIgnoreCase("all of them")) {
+score+=5;
+	JOptionPane.showMessageDialog(null, "Yay,  you were right! Your score is "+score+".");	
+}
+else {
+	score-=3;
+	JOptionPane.showMessageDialog(null, "You were wrong!");
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
-
+String answer1 =JOptionPane.showInputDialog("There is a boat filled with people, but there isn't a single person on board. How is this true?");
+if (answer1.equalsIgnoreCase("everyone onboard is married")) {
+score+=5;
+	JOptionPane.showMessageDialog(null, "Yay,  you were right! Your score is "+score+".");	
+}
+else {
+	score-=3;
+	JOptionPane.showMessageDialog(null, "You were wrong!");
+String answer2 =JOptionPane.showInputDialog("What does man love more than life, hate more than death or mortal strife; that which contented men desire; the poor have, the rich require; the miser spends, the spendthrift saves, and all men carry to their graves?");
+if (answer2.equalsIgnoreCase("nothing")) {
+score+=5;
+JOptionPane.showMessageDialog(null, "Yay,  you were right! Your score is "+score+".");	
+}
+else {
+score-=3;
+JOptionPane.showMessageDialog(null, "You were wrong!");
+}
 		// 2. Make a pop up to show the score.
 		
 	}
-}
+}}
 
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,21 @@
 
 void setup() {
+size(500, 500);
+
 
 }
 void draw() {
 
-}
+
+ PImage zombie = loadImage("download.jpeg");
+zombie.resize(width, height);
+image(zombie, 0, 0);
+
+fill(#FA0808);
+ellipse(305, 75, 50, 50);
+ellipse(225, 75, 50, 50);
+if(mouseX>=250){
+fill(#0A08FA);}
+if(mouseY>=250){
+  fill();
+}}
