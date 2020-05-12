7b2d20423f3055303d670eb6def44cc125252be8
root <root@a21201454685>
2020-05-12 22:30:19 +0000

    badger

--- src/_05_for_loops/_2_badgers/badgers.java
+++ src/_05_for_loops/_2_badgers/badgers.java
@@ -1,11 +1,13 @@
 package _05_for_loops._2_badgers;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 public class badgers {
 public static void main(String[] args) {
 	JOptionPane.showMessageDialog(null, "badger lyrics ;)");
-	for (int i = 0; i < 2; i++) {
+	for (int i3 = 0; i3 < 3; i3++) {
+	for (int i = 0; i < 3; i++) {
 		
 	
 	for (int i1 = 0; i1 < 12; i1++) {
@@ -14,7 +16,18 @@ public static void main(String[] args) {
 	}
 for (int j = 0; j < 2; j++) {
 	JOptionPane.showMessageDialog(null, "mushroom");
+
 }
 }
+
+for (int i2 = 0; i2 < 12; i2++) {
+	JOptionPane.showMessageDialog(null, "badger");}
+for (int i2 = 0; i2 < 5; i2++) {
+	JOptionPane.showMessageDialog(null, "snake");}
+	
 }
 }
+for (int i4 = 0; i4 < 12; i4++) {JOptionPane.showMessageDialog(null, "badger");}
+}
+
+
